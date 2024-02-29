35b737c73f4fd43761e1ba67bc03b52d54684dee
foreverpanda <fp4nd4@gmai.com>
2024-02-29 17:56:13 +0530

    Video effect done

--- script.js
+++ script.js
@@ -98,36 +98,121 @@ function loadingAnimation() {
     duration: 0.4,
   });
 
-  tl.from("#hero1, #page2",{
-    opacity:0,
-    duration:0.7
-  },"-=1.2")
+  tl.from(
+    "#hero1, #page2",
+    {
+      opacity: 0,
+      duration: 0.7,
+    },
+    "-=1.2"
+  );
 }
 function cursorAnimation() {
   document.addEventListener("mousemove", function (dets) {
     gsap.to("#crsr", {
-      left: dets.x-20,
-      top: dets.y-20,
+      left: dets.x - 20,
+      top: dets.y - 20,
     });
   });
 
-  Shery.makeMagnet("#nav-part2 h4",{
+  Shery.makeMagnet("#nav-part2 h4", {
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
-  })
-  ;
+    duration: 1,
+  });
+  var videoContainer = document.querySelector("#video-container");
+
+  var video = document.querySelector("#video-container video");
+
+  var flag = 0;
+
+  videoContainer.addEventListener("click", function () {
+    if (flag == 0) {
+      flag = 1;
+      video.play();
+      video.style.opacity = 1;
+      document.querySelector("#video-container img").style.opacity = 0;
+      document.querySelector(
+        "#video-cursor"
+      ).innerHTML = `<i class="ri-pause-line"></i>`;
+
+      gsap.to("#video-cursor", {
+        scale: 0.5,
+      });
+    } else {
+      flag = 0;
+      video.pause();
+      video.style.opacity = 0;
+      document.querySelector("#video-container img").style.opacity = 1;
+      document.querySelector(
+        "#video-cursor"
+      ).innerHTML = `<i class="ri-play-fill"></i>`;
+
+      gsap.to("#video-cursor", {
+        scale: 1,
+      });
+    }
+  });
+
+  videoContainer.addEventListener("mouseenter", function () {
+    videoContainer.addEventListener("mousemove", function (dets) {
+      gsap.to("#crsr", {
+        scale: 0,
+      });
+      gsap.to("#video-cursor", {
+        left: dets.x - 900,
+        top: dets.y - 900,
+      });
+    });
+  });
+  videoContainer.addEventListener("mouseleave", function () {
+    gsap.to("#crsr", {
+      scale: 1,
+    });
+    gsap.to("#video-cursor", {
+      left: "10%",
+      top: "30%",
+    });
+  });
 }
 loadingAnimation();
-// cursorAnimation();
+cursorAnimation();
 locomotiveAnimation();
 
-
-function sheryAnimation(){
-  Shery.imageEffect(".image-div",{
-    style:5,
-    config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272749998247967},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.61,"range":[0,10]},"metaball":{"value":0.35,"range":[0,2]},"discard_threshold":{"value":0.53,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
-    gooey:true
-
-  })
+function sheryAnimation() {
+  Shery.imageEffect(".image-div", {
+    style: 5,
+    config: {
+      a: { value: 2, range: [0, 30] },
+      b: { value: 0.75, range: [-1, 1] },
+      zindex: { value: -9996999, range: [-9999999, 9999999] },
+      aspect: { value: 0.7272749998247967 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: false },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: false },
+      maskVal: { value: 1, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 0 },
+      noise_speed: { value: 0.61, range: [0, 10] },
+      metaball: { value: 0.35, range: [0, 2] },
+      discard_threshold: { value: 0.53, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.5, range: [0, 2] },
+      noise_scale: { value: 10, range: [0, 100] },
+    },
+    gooey: true,
+  });
 }
 sheryAnimation();
